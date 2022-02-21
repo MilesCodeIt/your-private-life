e634c65c25549811e80d0512d34ffac86cfa9477
Mikkel RINGAUD <mikkel@milescode.dev>
2022-02-21 16:33:59 +0100

    Fix module not found

--- sources/pages/index.js
+++ sources/pages/index.js
@@ -1,16 +1,9 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Your Private Life</title>
-      </Head>
-
-      <main>
-        <h1>Projet en cours de développement !</h1>
-      </main>
-    </div>
+    <main>
+      <h1>Projet en cours de développement !</h1>
+    </main>
   );
 }
