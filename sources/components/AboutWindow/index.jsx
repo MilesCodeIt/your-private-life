a61c148f92f458e76c2b19d73b8d167d4e34fe21
Mikkel RINGAUD <vexitofficial@gmail.com>
2022-03-15 13:57:15 +0000

    Réparation de la fenetre About

--- sources/components/AboutWindow/index.jsx
+++ sources/components/AboutWindow/index.jsx
@@ -9,18 +9,26 @@ export default function AboutWindow ({ closeWindow }) {
 
   const mouseMoveEvent = (mouseEvent) => {
     if (!windowRef.current) return;
+
     mouseEvent.preventDefault();
     mouseEvent.stopPropagation();
 
-    const rect = windowRef.current.getBoundingClientRect();
+    const screenWidth = document.body.clientWidth;
+    const screenHeight = document.body.clientHeight;
 
-    const windowTop = mouseEvent.clientY - (mouseEvent.clientY - rect.top);
-    const windowLeft = mouseEvent.clientX - (mouseEvent.clientX - rect.left);
+    const mouseX = mouseEvent.clientX;
+    const mouseY = mouseEvent.clientY;
 
-    console.log(windowTop, windowLeft);
+    const windowTop = (mouseY + windowRef.current.clientHeight) < screenHeight
+      ? mouseY + (windowRef.current.clientHeight / 2)
+      : screenHeight - windowRef.current.clientHeight / 2;
 
-    windowRef.current.style.top = windowTop + "px";
-    windowRef.current.style.left = windowLeft + "px";
+    const windowLeft = (mouseX + windowRef.current.clientWidth / 2) < screenWidth
+      ? mouseX
+      : screenWidth - windowRef.current.clientWidth / 2;
+
+    windowRef.current.style.top = (mouseY >= 0 ? windowTop : windowRef.current.clientHeight / 2) + "px";
+    windowRef.current.style.left = (mouseX - windowRef.current.clientWidth / 2 >= 0 ? windowLeft : windowRef.current.clientWidth / 2) + "px";
   };
 
   const onMouseDownHandler = () => {
@@ -47,8 +55,15 @@ export default function AboutWindow ({ closeWindow }) {
         </a>
       </nav>
       <div className={styles.windowContent}>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt qui omnis quidem corporis reprehenderit, voluptates suscipit commodi dignissimos? Molestiae numquam similique voluptate soluta dolorum recusandae dolores ab placeat est animi?</p>
+        <h2>À propos</h2>
+        <p>
+          Projet pour les Trophées NSI proposé par des élèves du Lycée Turgot à Limoges.
+        </p>
 
+        <h3>But du projet</h3>
+        <p>
+          Prévention sur les risques liés aux cyberattaques.
+        </p>
       </div>
     </div>
   );
