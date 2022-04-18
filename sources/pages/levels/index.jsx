942e26f4efd0663cac2ad0b668d88fa7324a182b
Mikkel RINGAUD <mikkel@milescode.dev>
2022-04-18 11:02:24 +0200

    Ajout de l'introduction dans la liste des niveaux

--- sources/pages/levels/index.jsx
+++ sources/pages/levels/index.jsx
@@ -1,10 +1,30 @@
 import BrowserLayout from "@/components/BrowserLayout";
+import Link from "next/link";
+
+const levels = [
+  {
+    name: "Introduction",
+    description: "Tutoriel d'introduction à Your Private Life. Analysez vos mails et triez les. Attention, les spams ne sont pas triés !",
+    id: "introduction"
+  }
+];
 
 export default function LevelsIndex () {
 
   return (
     <BrowserLayout>
-      <h1>Hello World !</h1>
+      <h1>Niveaux</h1>
+
+      <div>
+        {levels.map(level => (
+          <Link key={level.id} href={`/levels/${level.id}`} passHref>
+            <a>
+              <h3>{level.name}</h3>
+              <span>{level.description}</span>
+            </a>
+          </Link>
+        ))}
+      </div>
     </BrowserLayout>
   );
 }
